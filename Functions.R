f4e45a04d2fe2f003c0cd438a32bc726a6a024d4
James Smith <james.smith@unsw.edu.au>
2024-09-20 15:20:53 +1000

    First commit

---
+++ Functions.R
@@ -0,0 +1,5 @@
+##############################################################
+####   Functions to generate and evaluate catch data...   ####
+####   ... given Site, Temperature, and Effort effects    ####
+####   J.A.Smith NSW DPI 30/8/24                          ####
+##############################################################
