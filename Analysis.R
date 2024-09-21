8692b1f87c6e8f4cf5bbd372d123acb9f3617d17
James Smith <james.smith@unsw.edu.au>
2024-09-21 15:40:17 +1000

    Finalised main script and functions

--- Analysis.R
+++ Analysis.R
@@ -7,16 +7,17 @@
 
 
 ## Load libraries
-library(mgcv) 
+library(mgcv)
+library(dplyr)
 
 ## Load functions
 source("Functions.R")
 
 
-## Generate the data scenarios
+## 1) Generate the data scenarios
 ## Here we generate 8 based on useful changes in collinearity, effort effects, and temp effects
 
-# Number of data points
+# Number of counts to generate
 n_obs <- 1200
 
 # The 8 scenarios as a data frame
@@ -39,10 +40,10 @@ for (dd in 1:8) {
                                beta_temp = 0.1,
                                seed=117)
   assign(paste0("data",dd), counts_dd)
-  
 }
 
-# Plot histograms for data scenarios; note that collinearity can change the magnitude of counts
+# Plot histograms for data scenarios
+# note that collinearity can change the magnitude of counts
 par(mfrow=c(3,3))
 for (dd in 1:8) {
   datax <- get(paste0("data",dd))
@@ -51,7 +52,8 @@ for (dd in 1:8) {
 }
 
 
-## For each data type, fit these six models
+## 2) For each data scenario, fit these six models
+
 fit_models <- function(data_num) {  #e.g. number suffix
   datax <- get(paste0("data",data_num))
   datax$logEffort <- log(datax$Effort)  #pre-transforming makes it easier to get Effort marginal effects
@@ -66,7 +68,62 @@ fit_models <- function(data_num) {  #e.g. number suffix
   return(list(M1=M1, M2=M2, M3=M3, M4=M4, M5=M5, M6=M6))
 }
 
-for (dd in 1:8) {  #each data scenario
-  assign(paste0("results_",dd), fit_models(data_type = dd))
+for (dd in 1:8) {
+  assign(paste0("results_",dd), fit_models(data_num = dd))
 }
 
+
+## 3) Extract results
+
+results <- save_plot_results(ylims=c(200, 200, 150, 150, 60, 200, 15, 50))
+
+
+## 4) Interpret results
+
+# Evaluate residuals
+for (dd in c(1:8)) {
+  resultsx <- get(paste0("results_",dd))
+  for (mm in 1:6) {
+    Mx <- resultsx[[paste0("M",mm)]]
+    plot(DHARMa::simulateResiduals(Mx),
+         main=paste0("Data=",dd," Model=",mm))
+  }
+}
+
+# MAE of all data
+save_mae <- results$save_mae
+wide_mae <- save_mae[save_mae$model %in% c(1:5),1:3] %>%  # *exclude M6 bc it's unfairly favoured in D5 and D8
+  tidyr::pivot_wider(names_from = model, values_from = MAE)
+wide_mae[, -1] <- t(apply(wide_mae[, -1], 1, function(x) {
+  100 * (x - min(x)) / min(x) } ))
+wide_mae <- round(as.data.frame(wide_mae), 2)  #percent change in MAE from minimum
+colMeans(wide_mae[wide_mae$data %in% c(1,2,5,6),])  #when effort effect is proportional
+colMeans(wide_mae[wide_mae$data %in% c(3,4,7,8),])  #when effort effect is threshold or constant
+
+# MAE > high Effort values
+wide_mae_max <- save_mae[save_mae$model %in% c(1:5),c(1,2,4)] %>%  # *exclude M6 bc it's unfairly favoured in D5 and D8
+  tidyr::pivot_wider(names_from = model, values_from = MAE_max)
+wide_mae_max[, -1] <- t(apply(wide_mae_max[, -1], 1, function(x) {
+  100 * (x - min(x)) / min(x) } ))
+wide_mae_max <- round(as.data.frame(wide_mae_max), 2)  #percent change in MAE from minimum
+colMeans(wide_mae_max[wide_mae_max$data %in% c(1,2,5,6),])  #when proportional
+colMeans(wide_mae_max[wide_mae_max$data %in% c(3,4,7,8),])  #when threshold or constant
+
+# MAE < low Effort values
+wide_mae_min <- save_mae[save_mae$model %in% c(1:5),c(1,2,5)] %>%  # *exclude M6 bc it's unfairly favoured in D5 and D8
+  tidyr::pivot_wider(names_from = model, values_from = MAE_min)
+wide_mae_min[, -1] <- t(apply(wide_mae_min[, -1], 1, function(x) {
+  100 * (x - min(x)) / min(x) } ))
+wide_mae_min <- round(as.data.frame(wide_mae_min), 2)  #percent change in MAE from minimum
+colMeans(wide_mae_min[wide_mae_min$data %in% c(1,2,5,6),])  #when proportional
+colMeans(wide_mae_min[wide_mae_min$data %in% c(3,4,7,8),])  #when threshold or constant
+
+# Estimates - did they recover the true effect?
+# These summaries are = 1 if 95% CI overlaps the true estimate, and = 0 otherwise
+estimate_summary <- estimate_accuracy(results = results)
+estimate_summary[estimate_summary$model==1,]  #examine each model
+
+# Smoothers - did they recover the true effect?
+prop_effort_smoothers()  #proportional and constant effort effects: red line should be within CIs
+thresh_effort_smoothers()  #threshold effort effects: red line should be within CIs
+temp_effort_smoothers()  #the temp smoother: red line should be within CIs; visually inspect domed effects
--- Functions.R
+++ Functions.R
@@ -119,3 +119,275 @@ generate_counts <- function(n_obs = 1000,  #number of counts to generate
                     Temperature = temperature,
                     Effort = effort))
 }
+
+
+## Code to extract and plot some results
+## I wrote this as a function to keep the main script cleaner
+
+save_plot_results <- function(ylims) {
+  
+  # Write data frames to save data
+  # Estimates and SEs (don't save smoothers here)
+  save_coefs <- expand.grid(data = 1:8, model=1:6)
+  save_coefs$Intercept <- NA; save_coefs$Temperature <- NA
+  save_coefs$SiteB <- NA; save_coefs$SiteC <- NA
+  save_coefs$logEffort <- NA; save_coefs$Effort <- NA
+  save_SEs <- save_coefs
+  
+  # MAE (or could use RMSE)
+  save_mae <- expand.grid(data = 1:8, model=1:6)
+  save_mae$MAE <- NA  #all data
+  save_mae$MAE_max <- NA  #high effort value data
+  save_mae$MAE_min <- NA  #low effort value data
+  
+  par(mfrow=c(4,2), mar=c(2.5,3,2,1))
+  colx <- c("red","blue","darkgreen","purple","green","orange")
+  ylims <- ylims  #ylim for plot of each data scenario
+  for (dd in 1:8) {  #data scenarios
+    
+    resultsx <- get(paste0("results_",dd))
+    datax <- get(paste0("data",dd))
+    
+    for (mm in 1:6) {  #model types
+      
+      Mx <- resultsx[[paste0("M",mm)]]
+      rowx <- which(save_mae$data==dd & save_mae$model==mm)
+      
+      # Estimates and SEs
+      coefsx <- Mx$coefficients[1:5]
+      SEsx <-  summary(Mx)$se[1:5]
+      clean_names <- gsub("[()]", "", names(coefsx))
+      clean_names <- clean_names[clean_names %in% names(save_coefs)]
+      save_coefs[rowx,clean_names] <- coefsx[which(clean_names %in% names(save_coefs))]
+      save_SEs[rowx,clean_names] <- SEsx[which(clean_names %in% names(save_SEs))]
+      
+      # MAE, in this case > 15 units is high effort, < 8 units low effort
+      save_mae$MAE[rowx] <- MAE(Mx$fitted.values, datax$Count)
+      save_mae$MAE_max[rowx] <- MAE(Mx$fitted.values[datax$Effort > 15], datax$Count[datax$Effort > 15])
+      save_mae$MAE_min[rowx] <- MAE(Mx$fitted.values[datax$Effort < 8], datax$Count[datax$Effort < 8])
+      
+      # Marginal effort effects
+      effort_seq <- seq(4, 20, length.out = 100)
+      log_effort_seq <- log(effort_seq)
+      new_data <- data.frame(Site="A",
+                             Temperature=20,
+                             Effort=effort_seq,
+                             logEffort=log_effort_seq)
+      pred <- predict(Mx, newdata=new_data, type="response")
+      
+      # Plot the marginals (this looks complicated due to necessary changes in how the 'true effect' is plotted)
+      if (mm == 1) {
+        plot(effort_seq, pred, type='l', ylim=c(0,ylims[dd]), xlim=c(4,20),
+             xlab="Effort", ylab="Count", lwd=2, las=1,
+             main=paste0("Effort marginal, data=",dd), col=colx[mm])
+        legend("topleft", legend=c("model1","model2","model3","model4","model5","model6"),
+               col=colx, lty=1:6, lwd=2, cex=0.8)
+        if (dd %in% c(1,2,6)) {  #add proportional 'true effect'
+          true_effect <- exp(0 + 0 + 0.1 * mean(datax$Temperature)) * effort_seq
+          lines(effort_seq, true_effect, lwd=3, col="darkgrey")
+        }
+        if (dd == 1) { points(data1$Effort, data1$Count, col="grey", pch=20, cex=0.8) }
+        if (dd %in% c(3,4,8)) {  #add threshold 'true effect'
+          effort_threshold <- 12  #same as in the 'generate_counts' function
+          median_effort <- median(datax$Effort)
+          save_preds <- rep(0,6)
+          for (mm in 1:6) {  #get mean prediction from all models
+            Mxx <- resultsx[[paste0("M",mm)]]
+            predx <- predict(Mxx, newdata=new_data, type="response")
+            save_preds[mm] <- predx[which.min(abs(new_data$Effort - median_effort))]
+          }
+          rescale_factor <- mean(save_preds) / (min(median_effort, effort_threshold))
+          true_effect <- rescale_factor * pmin(effort_seq, effort_threshold)
+          lines(effort_seq, true_effect, lwd=3, col="darkgrey")
+        }
+        if (dd == 7) {
+          true_effect <- mean(datax$Count)
+          lines(effort_seq, rep(true_effect, length(effort_seq)), lwd=3, col="darkgrey")
+        }
+      } else {
+        lines(effort_seq, pred, col=colx[mm], lty=mm, lwd=2)
+      }
+    }
+  }
+  return(list(save_coefs = save_coefs,
+         save_SEs = save_SEs,
+         save_mae = save_mae))
+}
+
+
+## Code to measure whether the 95% CI of an estimate encompasses the true value
+## I wrote this as a function to keep the main script cleaner
+
+estimate_accuracy <- function(results) {
+  
+  save_coefs <- results$save_coefs
+  save_SEs <- results$save_SEs
+  summary_in_out <- save_coefs
+  for (vv in c("Intercept","Temperature","SiteB","SiteC","logEffort","Effort")) {
+    if (vv == "Intercept") { target <- 0 }  #the true values
+    if (vv == "Temperature") { target <- 0.1 }
+    if (vv == "SiteB") { target <- 0.5 }
+    if (vv == "SiteC") { target <- -0.5 }
+    if (vv == "logEffort") { target <- 1 }
+    if (vv == "Effort") { target <- 0 }
+    for (rr in 1:nrow(save_coefs)) {
+      coefx <- save_coefs[rr,vv]
+      SEx <- save_SEs[rr,vv]
+      if (is.na(coefx) == F) {
+        lower_ci <- coefx - SEx*1.96
+        upper_ci <- coefx + SEx*1.96
+        if (lower_ci <= target && upper_ci >= target) {
+          summary_in_out[rr,vv] <- 1
+        } else {
+          summary_in_out[rr,vv] <- 0
+        }
+      }
+    }
+  }
+  return(summary_in_out)
+  
+}
+
+
+## Code to plot and evaluate whether the models with effort smoothers...
+## ...accurately recover the proportional effort effects
+## I wrote this as a function to keep the main script cleaner
+
+prop_effort_smoothers <- function() {
+  
+  # Evaluate Effort smoother in M4, M5 & M6
+  # Successful if red line (prop. Effort effect is within CIs of GAM)
+  par(mfrow=c(2,2), mar=c(4,4,3,1))
+  for (dd in c(1,2,5,6,7)) {
+    resultsx <- get(paste0("results_",dd))
+    for (mm in c(4,6)) {
+      Mx <- resultsx[[paste0("M",mm)]]
+      px <- ggeffects::predict_response(Mx, terms = c("logEffort"), 
+                                        condition = c(Temperature=20, Site="A"))
+      plot(exp(px$x), px$predicted, type="l",
+           ylim=c(0,max(px$conf.high)), main=paste0("M",mm," - Data ",dd), las=1,
+           ylab="Count", xlab="Effort")
+      lines(exp(px$x), px$conf.low, lty=2)
+      lines(exp(px$x), px$conf.high, lty=2)
+      if (dd == 7) { #no effort effect - slope = 0
+        idx <- which.min(abs(px$x - log(12)))  #find a point near middle that GAM response runs through
+        mid_pointx <- px$x[idx]
+        mid_pointy <- px$predicted[idx]
+        lines(exp(px$x), rep(mid_pointy, length(px$x)), col="red")
+      } else {
+        idx <- which.min(abs(px$x - log(12)))  #find a point near middle that GAM response runs through
+        mid_pointx <- px$x[idx]
+        mid_pointy <- px$predicted[idx]
+        intercept <- log(mid_pointy) - log(12) - 0.1*20
+        lines(exp(px$x), exp(intercept + 0.1*20 + px$x), col="red")
+      }
+    }
+    for (mm in c(5)) {
+      Mx <- resultsx[[paste0("M",mm)]]
+      px <- ggeffects::predict_response(Mx, terms = c("Effort"), 
+                                        condition = c(Temperature=20, Site="A"))
+      plot(px$x, px$predicted, type="l",
+           ylim=c(0,max(px$conf.high)), main=paste0("M",mm," - Data ",dd), las=1,
+           ylab="Count", xlab="Effort")
+      lines(px$x, px$conf.low, lty=2)
+      lines(px$x, px$conf.high, lty=2)
+      if (dd == 7) { #no effort effect - slope = 0
+        idx <- which.min(abs(px$x - 12))  #find a point near middle that GAM response runs through
+        mid_pointx <- px$x[idx]
+        mid_pointy <- px$predicted[idx]
+        lines(px$x, rep(mid_pointy, length(px$x)), col="red")
+      } else {
+        idx <- which.min(abs(px$x - 12))  #find a point near middle that GAM response runs through
+        mid_pointx <- px$x[idx]
+        mid_pointy <- px$predicted[idx]
+        intercept <- log(mid_pointy) - log(12) - 0.1*20
+        lines(px$x, exp(intercept + 0.1*20 + log(px$x)), col="red")
+      }
+    }
+  }
+  
+}
+
+
+## Code to plot and evaluate whether the models with effort smoothers...
+## ...accurately recover the threshold effort effects
+## I wrote this as a function to keep the main script cleaner
+
+thresh_effort_smoothers <- function() {
+  
+  par(mfrow=c(2,2), mar=c(4,4,3,1))
+  for (dd in c(3,4,8)) {
+    datax <- get(paste0("data",dd))
+    resultsx <- get(paste0("results_",dd))
+    for (mm in c(4,6)) {
+      Mx <- resultsx[[paste0("M",mm)]]
+      px <- ggeffects::predict_response(Mx, terms = c("logEffort"), 
+                                        condition = c(Temperature=20, Site="A"))
+      plot(exp(px$x), px$predicted, type="l",
+           ylim=c(0,max(px$conf.high)), main=paste0("M",mm," - Data ",dd), las=1,
+           ylab="Count", xlab="Effort")
+      lines(exp(px$x), px$conf.low, lty=2)
+      lines(exp(px$x), px$conf.high, lty=2)
+      effort_threshold <- 12  #previously max(datax$Effort)/2
+      true_effect <- exp(0 + 0.1 * 20) * pmin(exp(px$x), effort_threshold)
+      if (dd == 8) {  #needs rescaling due to strong collinearity
+        median_effort <- median(datax$Effort)
+        median_pred <- px$predicted[which.min(abs(exp(px$x) - median_effort))]
+        rescale_factor <- median_pred / (min(median_effort, effort_threshold))
+        true_effect <- rescale_factor * pmin(exp(px$x), effort_threshold)
+      }
+      lines(exp(px$x), true_effect, col="red")
+    }
+    for (mm in c(5)) {
+      Mx <- resultsx[[paste0("M",mm)]]
+      px <- ggeffects::predict_response(Mx, terms = c("Effort"), 
+                                        condition = c(Temperature=20, Site="A"))
+      plot(px$x, px$predicted, type="l",
+           ylim=c(0,max(px$conf.high)), main=paste0("M",mm," - Data ",dd), las=1,
+           ylab="Count", xlab="Effort")
+      lines(px$x, px$conf.low, lty=2)
+      lines(px$x, px$conf.high, lty=2)
+      effort_threshold <- 12  #previousy max(datax$Effort)/2
+      true_effect <- exp(0 + 0.1 * 20) * pmin(px$x, effort_threshold)
+      if (dd == 8) {
+        median_effort <- median(datax$Effort)
+        median_pred <- px$predicted[which.min(abs(px$x - median_effort))]
+        rescale_factor <- median_pred / (min(median_effort, effort_threshold))
+        true_effect <- rescale_factor * pmin(px$x, effort_threshold)
+      }
+      lines(px$x, true_effect, col="red")
+    }
+  }
+  
+}
+
+
+## Code to plot and evaluate whether the model with a temperature smoother...
+## ...could accurately recover the temperature effect
+## I wrote this as a function to keep the main script cleaner
+
+temp_effort_smoothers <- function() {
+  
+  par(mfrow=c(2,2))
+  for (dd in c(1:4,6:7)) {
+    resultsx <- get(paste0("results_",dd))
+    Mx <- resultsx[["M6"]]
+    px <- ggeffects::predict_response(Mx, terms = c("Temperature"), 
+                                      condition = c(logEffort=log(12), Site="A"))
+    plot(px$x, px$predicted, type="l", ylim=c(0,max(px$conf.high)),
+         main=paste("M6 - Data",dd), las=1, ylab="Count", xlab="Temperature")
+    lines(px$x, px$conf.low, lty=2)
+    lines(px$x, px$conf.high, lty=2)
+    idx <- which.min(abs(px$x - 20))  #find a point near middle that GAM response runs through
+    mid_pointx <- px$x[idx]
+    mid_pointy <- px$predicted[idx]
+    intercept <- log(mid_pointy) - log(12) - 0.1*mid_pointx
+    lines(px$x, exp(intercept + 0.1*px$x + log(12)), col="red")
+  }
+  for (dd in c(5,8)) {  #visually inspect Temp smoothers
+    resultsx <- get(paste0("results_",dd))
+    Mx <- resultsx[["M6"]]
+    plot(Mx, select=1, main=paste0("M6, Data",dd))
+  }
+  
+}
